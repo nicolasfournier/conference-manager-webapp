cd8150ee0b9cc87a597cd592fdb029111e28e8f5
Nicolas Fournier <nicolasfournier@yahoo.com>
2025-02-14 08:17:59 +0100

    first exercises

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,48 @@
-import { useEffect, useState } from "react";
-import type { Schema } from "../amplify/data/resource";
-import { generateClient } from "aws-amplify/data";
-import { Authenticator } from "@aws-amplify/ui-react";
+//import { useEffect, useState } from "react";
+//import type { Schema } from "../amplify/data/resource";
+//import { generateClient } from "aws-amplify/data";
+//import { Authenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
 
-const client = generateClient<Schema>();
+type itemType={title: string,author:string,year:number,number_reviews:number,stars:number,id:number};
+//const client = generateClient<Schema>();
+const testdata:itemType[] =
+  [
+    {
+      title: 'random Title 1',
+      author: 'frodo',
+      year: 1750,
+      number_reviews: 3,
+      stars: 4,
+      id: 31,
+    },
+    {
+      title: 'random Title 2',
+      author: 'bilbo',
+      year: 1770,
+      number_reviews: 40,
+      stars: 5,
+      id: 20,
+    },
+  ];
+
+function itemlayout(item:itemType) {
+  return (<li> title {item.title} - author {item.author} </li>);
+}
+const List = ()=>{
+  return (<ul> {testdata.map((item)=>itemlayout(item))}</ul>);
+}
 
 function App() {
+  return (
+  <div>
+    <h1>Testpage React</h1>
+    <label htmlFor="search">Search:</label>
+    <input id="search" type="text"></input>
+    <hr />
+    <List />
+  </div>);
+  /*
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
   useEffect(() => {
@@ -48,6 +84,7 @@ function App() {
       )}
     </Authenticator>
   );
+*/
 }
 
 export default App;
