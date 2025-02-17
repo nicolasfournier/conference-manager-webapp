d0d12a38ea13648cb5712d2e07ab742f5d847628
Nicolas Fournier <nicolasfournier@yahoo.com>
2025-02-17 20:52:24 +0100

    object destructuring and capitalisation of types

--- src/App.tsx
+++ src/App.tsx
@@ -7,154 +7,107 @@ import "@aws-amplify/ui-react/styles.css";
 
 //const client = generateClient<Schema>();
 
-type bookType={title: string,author:string,url:string,year:number,number_reviews:number,stars:number,id:number};
+type BookType = { title: string, author: string, url: string, year: number, number_reviews: number, stars: number, id: number }
 
-type bookListPropsType = {
-  list:bookType[]; 
-}
-
-type bookPropsType = {
-  //note: there can be more arguments passed originally, but only those in the propstype will be accessible
-  //so the "key" might be given in the original call of the <Book /> tag, but the "key" property need not be defined in the bookPropsType
-  //It is then not accessible in <Book />
-  //  key: number;
-  book: bookType;
-}
-
-const Book = (bookProps:bookPropsType) => {
+//note: there can be more arguments passed originally, but only those in the propstype will be accessible
+//so the "key" might be given in the original call of the <Book /> tag, but the "key" property need not be defined in the bookPropsType
+//It is then not accessible in <Book />
+//  key: number;
+type BookPropsType = { book: BookType }
+const Book = ({ book }: BookPropsType) => {
   return (
-  <li key={bookProps.book.id}> 
-    <span>Title: <a href={bookProps.book.url}>{bookProps.book.title}</a></span>
-    <span> - - - </span>
-    <span>Author: <b>{bookProps.book.author}</b></span>
-  </li>);
+    <li key={book.id}>
+      <span>Title: <a href={book.url}>{book.title}</a></span>
+      <span> - - - </span>
+      <span>Author: <b>{book.author}</b></span>
+    </li>);
 }
 
-const BookList = (props:bookListPropsType) => {
-  return (<ul> {props.list.map((book)=><Book key={book.id} book={book}/>)}</ul>);
+/*
+//Version of parameter passing with object destructuring.  
+// Not nice.  I don't like it much.  I don't see the advantage
+type book2PropsType = { title: string, author: string, url: string, id: number }
+const Book2 = ({ title, author, url, id }: book2PropsType) => {
+  return (
+    <li key={id}>
+      <span>Title: <a href={url}>{title}</a></span>
+      <span> - - - </span>
+      <span>Author: <b>{author}</b></span>
+    </li>);
+}
+*/
+
+type BookListPropsType = { list: BookType[] }
+const BookList = ({ list }: BookListPropsType) => {
+  return (<ul> {list.map((book) => <Book key={book.id} book={book} />)}</ul>);
+  //  return (<ul> {list.map((book)=><Book2 key={book.id} title={book.title}  author={book.author}  url={book.url}  id={book.id}/>)}</ul>);
 }
 
-const PageTitle = () => {
+type PageTitlePropsType = { title: string }
+const PageTitle = ({ title }: PageTitlePropsType) => {
   return (
     <div>
-      <h1>Testpage React</h1>
+      <h1>{title}</h1>
     </div>
   );
 }
 
-type searchPropsType = {
-  onSearch: (event:React.ChangeEvent<HTMLInputElement>)=> void;
-}
-
-const SearchField = (searchProps:searchPropsType) => {
-  // const handleChange = (event:React.ChangeEvent<HTMLInputElement>) => { /* (event: { target: { value: any; }; }) => {*/
-  //   console.log("event");
-  //   console.log(event);
-  //   console.log("target");
-  //   console.log(event.target);
-  //   console.log("value");
-  //   console.log(event.target.value);
-  //   searchProps.onSearch(event);
-  // }
-    return (
+type SearchPropsType = { onSearch: (event: React.ChangeEvent<HTMLInputElement>) => void }
+const SearchField = ({ onSearch }: SearchPropsType) => {
+  return (
     <div>
       <label htmlFor="search">Search:</label>
-      <input id="search" type="text" onChange={searchProps.onSearch}></input>
+      <input id="search" type="text" onChange={onSearch}></input>
     </div>
   );
 }
 
 function App() {
-  const booklist:bookType[] =
-  [
-    {
-      title: 'lord of the rings',
-      author: 'sauron',
-      url: 'https://de.wikipedia.org/wiki/Sauron',
-      year: 1750,
-      number_reviews: 3,
-      stars: 4,
-      id: 1,
-    },
-    {
-      title: 'the hobbit',
-      author: 'bilbo',
-      url: 'https://de.wikipedia.org/wiki/Hobbit',
-      year: 1770,
-      number_reviews: 40,
-      stars: 5,
-      id: 2,
-    },
-    {
-      title: 'the ring bearer',
-      author: 'frodo',
-      url: 'https://de.wikipedia.org/wiki/Figuren_in_Tolkiens_Welt#Frodo_Beutlin',
-      year: 1790,
-      number_reviews: 4,
-      stars: 4.5,
-      id: 3,
-    },
-  ];
+  const booklist: BookType[] =
+    [
+      {
+        title: 'lord of the rings',
+        author: 'sauron',
+        url: 'https://de.wikipedia.org/wiki/Sauron',
+        year: 1750,
+        number_reviews: 3,
+        stars: 4,
+        id: 1,
+      },
+      {
+        title: 'the hobbit',
+        author: 'bilbo',
+        url: 'https://de.wikipedia.org/wiki/Hobbit',
+        year: 1770,
+        number_reviews: 40,
+        stars: 5,
+        id: 2,
+      },
+      {
+        title: 'the ring bearer',
+        author: 'frodo',
+        url: 'https://de.wikipedia.org/wiki/Figuren_in_Tolkiens_Welt#Frodo_Beutlin',
+        year: 1790,
+        number_reviews: 4,
+        stars: 4.5,
+        id: 3,
+      },
+    ];
   const [searchState, setSearchState] = React.useState('');
-  const searchChangeHandler = (event:React.ChangeEvent<HTMLInputElement>) => {
+  const searchChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
     console.log(event.target.value);
     setSearchState(event.target.value);
   }
 
-  const filteredBooks = booklist.filter((book)=>{return book.title.toLowerCase().includes(searchState.toLowerCase())});
+  const filteredBooks = booklist.filter((book) => { return book.title.toLowerCase().includes(searchState.toLowerCase()) });
 
   return (
-  <div>
-    <PageTitle />
-    <SearchField onSearch={searchChangeHandler}/>
-    <hr />
-    <BookList list={filteredBooks} />
-  </div>);
+    <div>
+      <PageTitle title="React Testpage Title" />
+      <SearchField onSearch={searchChangeHandler} />
+      <hr />
+      <BookList list={filteredBooks} />
+    </div>);
 }
-  /*
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-
-  useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }, []);
-
-  function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
-  }
-
-  function deleteTodo(id: string) {
-    client.models.Todo.delete({ id });
-  }
-
-  function App() {
-  return (
-    <Authenticator>
-      {({ signOut, user }) => (
-        <main>
-          <h1>{user?.signInDetails?.loginId}'s todos</h1>
-          <button onClick={createTodo}>+ new</button>
-          <ul>
-            {todos.map((todo) => (
-              <li key={todo.id} onClick={() => deleteTodo(todo.id)}>
-                {todo.content}
-              </li>
-            ))}
-          </ul>
-          <div>
-            🥳 App successfully hosted. Try creating a new todo.
-            <br />
-            <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-              Review next step of this tutorial.
-            </a>
-          </div>
-          <button onClick={signOut}>Sign out</button>
-        </main>
-      )}
-    </Authenticator>
-  );
-}
-*/
 
 export default App;
